0d7be8d9793d4427e599b6ae6df34cc4264ce3e8
ttalpsep <ttalpsep@itcollege.ee>
2012-10-14 19:24:03 +0300

    ylesanne-1

---
+++ src/main/GuardPost.java
@@ -0,0 +1,28 @@
+package main;
+
+public class GuardPost {
+	private String name;
+	private String address;
+	private int guardCount;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public int getGuardCount() {
+		return guardCount;
+	}
+	public void setGuardCount(int count) {
+		this.guardCount = count;
+	}
+
+}
