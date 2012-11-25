6a3d5cd323c09f782abb52584b009c9962edb703
ttalpsep <ttalpsep@itcollege.ee>
2012-11-25 05:03:11 +0200

    Incident modify, delete; GuardController koos kõigi funktsioonidega ja jsp-dega.

--- src/main/dao/IncidentDAO.java
+++ src/main/dao/IncidentDAO.java
@@ -1,10 +1,14 @@
 package main.dao;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import main.data.Guard;
 import main.data.Incident;
 import manage.Management;
 
@@ -24,12 +28,41 @@ public class IncidentDAO {
     }
 
     public void insertIncident(Incident incident) {
-        em.persist(incident);
+    	if (em.find(Incident.class,incident.getId())!=null){
+    		em.merge(incident);	
+    	} else {
+    		em.persist(incident);
+    	}
     }
 
     public void terminate() {
         em.getTransaction().commit();
         em.close();
     }
+    
+    public void modifyIncident(Incident in) {
+        /*private Date open;
+        @DateTimeFormat(pattern="dd.MM.yyyy")
+        private Date close;
+        private String location;
+        private String description;
+        private int involvedGuardCount;
+        private String status;*/
+        Incident in1 = em.find(Incident.class,in.getId());
+        in1=in;
+        em.merge(in1);
+    }
+    
+    public Incident getIncident(int id) {
+        return em.find(Incident.class,id);
+    }
+    
+    public void deleteIncident(int id) {
+        Incident in=em.find(Incident.class,id);
+        em.remove(in);
+        
+    }
+    
+
 
 }
